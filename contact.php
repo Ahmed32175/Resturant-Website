86aa6604660447d478b4a5f1a93fbcfc91b37e59
Ahmed Aslam <ahmedaslam2004@gmail.com>
2024-08-09 18:30:14 -0400

    syntax error fix, view menu button add

--- contact.php
+++ contact.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <html lang ="en-US">
+	<head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="styles.css">
-
+<title>Contact</title>
+</head>
 <body>
-	<title>Contact</title>
+	
 <div id="container">
 
 
